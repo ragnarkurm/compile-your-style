7ddede1b51c453475cc8da4b0bfcc9a57194cf70
Ragnar Kurm <ragnar.kurm@gmail.com>
2013-08-26 12:47:43 +0300

    delete identical .sass files, instead provide template .sass which can be copied where needed; files.mk simplified according to these changes. deleted rudimentary css/img.sass which now comes from img/ automatically

--- css/files.mk
+++ css/files.mk
@@ -1,71 +1,84 @@
 ELEMENTS = $(addprefix elements/, \
-	tags \
-	\
-	image \
-	logo \
-	system.messages \
 )
+
+#	tags \
+#	\
+#	image \
+#	logo \
+#	system.messages \
+
 
 STRUCTURAL = $(addprefix structural/, \
-	zones \
-	regions \
-	blocks \
-	\
-	zone-content \
-	zone-footer \
-	zone-header \
-	\
-	region-content \
-	region-footer-first \
-	region-header-first \
-	region-sidebar-first \
-	\
-	block-logo \
-	block-menu-main \
-	block-system-main \
-	block-tabs \
 )
+
+#	zones \
+#	regions \
+#	blocks \
+#	\
+#	zone-content \
+#	zone-footer \
+#	zone-header \
+#	\
+#	region-content \
+#	region-footer-first \
+#	region-header-first \
+#	region-sidebar-first \
+#	\
+#	block-logo \
+#	block-menu-main \
+#	block-system-main \
+#	block-tabs \
+
 
 FIELDS = $(addprefix fields/, \
-	form \
-	type-display \
-	type-form \
-	\
-	type--boolean--display \
-	type--boolean--form \
-	type--decimal--display \
-	type--decimal--form \
-	type--long-text-and-summary--display \
-	type--long-text-and-summary--form \
-	\
-	field--contacts--display \
-	field--contacts--form \
-	field--main-image--display \
-	field--main-image--form \
-	field--options--display \
-	field--options--form \
 )
+
+#	form \
+#	type-display \
+#	type-form \
+#	\
+#	type--boolean--display \
+#	type--boolean--form \
+#	type--decimal--display \
+#	type--decimal--form \
+#	type--long-text-and-summary--display \
+#	type--long-text-and-summary--form \
+#	\
+#	field--contacts--display \
+#	field--contacts--form \
+#	field--main-image--display \
+#	field--main-image--form \
+#	field--options--display \
+#	field--options--form \
+
 
 VIEWS = $(addprefix views/, \
-	views \
-	\
-	display-grid \
-	display-table \
-	\
-	articles \
-	products \
 )
+
+#	views \
+#	\
+#	display-grid \
+#	display-table \
+#	\
+#	articles \
+#	products \
+
 
 NODE = $(addprefix node/, \
-	node \
-	\
-	article \
-	book \
-	page \
 )
 
+#	node \
+#	\
+#	article \
+#	book \
+#	page \
+
+
 PAGES = $(addprefix pages/, \
-	contact \
-	front \
-	node-add \
 )
+
+#	contact \
+#	front \
+#	node-add \
+
+
